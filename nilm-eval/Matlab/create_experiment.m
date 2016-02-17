b1ab9bf9885571da15d413d99be1544f2995747f
RuneHeick <hteml@hotmail.com>
2016-02-17 12:15:39 +0100

    Created new data

--- nilm-eval/Matlab/create_experiment.m
+++ nilm-eval/Matlab/create_experiment.m
@@ -8,9 +8,9 @@
 function create_experiment()
 
     %% SPECIFY CONFIGURATION AND EXPERIMENT
-    configuration_input = 'input/configurations/parsonAppliance_initial.yaml';
+    configuration_input = 'input/configurations/weiss_initial.yaml';
     %experiment_input = 'input/experiments/weiss/app_laptop_r_0.2.yaml';
-    experiment_input = 'input/experiments/parson/Ex2/fridge_EX1.yaml';
+    experiment_input = 'input/experiments/weiss/WeissErrorEx3.yaml';
 
     % load values of configuration and experiment
     configuration = ReadYaml(configuration_input);
